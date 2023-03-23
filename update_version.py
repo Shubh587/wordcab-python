1e0e2cf493798a72f5e4daf16fc7b866b3e1e504
ChainYo <thomas@wordcab.com>
2023-03-23 20:11:08 +0100

    fix update version

--- update_version.py
+++ update_version.py
@@ -2,23 +2,31 @@
 import re
 
 
-init_file = "wordcab/__init__.py"
+init_file = "src/wordcab/__init__.py"
 version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
 
 with open(init_file) as f:
     content = f.read()
+    previous_version = re.search(version_regex, content, re.M)
 
-version = re.search(version_regex, content, re.M)
+project_toml = "pyproject.toml"
+toml_version_regex = r"^version = ['\"]([^'\"]*)['\"]"
 
-if version:
-    current_version = version.group(1)
-    print(f"Current version: {current_version}")
-    new_version = input("Enter new version: ")
-    content = re.sub(version_regex, f"__version__ = {new_version!r}", content, re.M)
+with open(project_toml) as f:
+    toml_content = f.read()
+    toml_version = re.search(toml_version_regex, content, re.M)
 
-    with open(init_file, "w") as f:
-        f.write(content)
-        print(f"Updated {init_file} with version: {new_version}")
+if previous_version and toml_version:
+    if previous_version.group(1) == toml_version.group(1):
+        print(f"Version {previous_version.group(1)} already up to date")
+        exit(0)
+    else:
+        new_version = toml_version.group(1)
+        content = re.sub(version_regex, f"__version__ = {new_version!r}", content, re.M)
+
+        with open(init_file, "w") as f:
+            f.write(content)
+            print(f"Updated {init_file} with version: {new_version}")
 
 else:
     raise RuntimeError("Unable to find __version__ string in __init__.py")
